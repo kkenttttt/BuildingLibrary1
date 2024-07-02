1a0fc7236584d972c3769db73b565d79fc9b9ec5
Елизавета <lizalizapotter123@gmail.com>
2024-07-03 00:34:23 +0300

    Добавьте файлы проекта.

---
+++ BuildingConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuildingLibrary;
+
+namespace BuildingConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building building = new Building("ул. Ленина, 15", "Жилой дом");
+            //работу выполнила Корчагина Елизавета
+            building.AddApartment(new Apartment(1, "John Doe"));
+            building.AddApartment(new Apartment(2, "Jane Doe"));
+            building.AddApartment(new Apartment(3, "Jim Smith"));
+            building.ShowAll();
+            Console.ReadKey(); //работу выполнила Корчагина Елизавета
+
+        }
+    }
+}
---
+++ BuildingLibrary/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingLibrary
+{
+    public class Building
+    {
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public List<Apartment> Apartments { get; set; }
+
+        public Building(string address, string description)
+        {
+            Address = address;
+            Description = description;
+            Apartments = new List<Apartment>();
+        }
+        //работу выполнила Корчагина Елизавета
+
+        public void AddApartment(Apartment apartment)
+        {
+            Apartments.Add(apartment);
+        }
+        //работу выполнила Корчагина Елизавета
+
+        public void ShowAll()
+        {
+            Apartments.Sort();
+            foreach (var apartment in Apartments)
+            {
+                apartment.Show();
+            }
+        }
+    }
+    //работу выполнила Корчагина Елизавета
+    public class Apartment : IComparable
+    {
+        public int Number { get; set; }
+        public string Owner { get; set; }
+        public Apartment(int number, string owner)
+        {
+            Number = number;
+            Owner = owner;
+        }
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            Apartment apartment = obj as Apartment;
+            if (apartment != null)
+            {
+                return this.Number.CompareTo(apartment.Number);
+            }
+            else
+            {
+                throw new ArgumentException("Object is not an Apartment");
+            }
+        }
+        //работу выполнила Корчагина Елизавета
+        public void Show()
+        {
+            Console.WriteLine($"Number: {Number}, Owner: {Owner}");
+        }
+        //работу выполнила Корчагина Елизавета
+    }
+}
